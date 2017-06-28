8d3f667c14febd39880bf9e35df825b3d81d80db
MSA! <MSAteam0@gmail.com>
2017-06-28 11:19:54 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
- -define('BOT_TOKEN', '**TOKEN**');
+ -define('BOT_TOKEN', '278015234:AAGelEpCcKTPUuo4Ep0CNztb5AW7yWPi0ho');
  -define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
  -
  -function apiRequestWebhook($method, $parameters) {
@@ -115,7 +115,7 @@
  -  // process incoming message
  -  $boolean = file_get_contents('booleans.txt');
  -  $booleans= explode("\n",$boolean);
- -  $admin = **ADMIN**;
+ -  $admin = 99933234;
  -  $message_id = $message['message_id'];
  -  $rpto = $message['reply_to_message']['forward_from']['id'];
  -  $chat_id = $message['chat']['id'];
